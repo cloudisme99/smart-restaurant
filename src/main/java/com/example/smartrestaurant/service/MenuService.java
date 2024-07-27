9ea1d3526e114f7312deee1ebdf1b4a8bdc12d05
HEO <cloudisme99@gmail.com>
2024-07-27 21:24:03 +0900

    Feat: Menu 수정 기능 구현

--- src/main/java/com/example/smartrestaurant/service/MenuService.java
+++ src/main/java/com/example/smartrestaurant/service/MenuService.java
@@ -4,6 +4,7 @@ import com.example.smartrestaurant.domain.Menu;
 import com.example.smartrestaurant.dto.MenuDto;
 import com.example.smartrestaurant.repository.MenuRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,5 +32,17 @@ public class MenuService {
 		return menuRepository.findAll().stream()
 			.map(MenuDto::fromEntity)
 			.collect(Collectors.toList());
+	}
+
+	@Transactional
+	public Optional<MenuDto> updateMenu(Long id, MenuDto menuDto) {
+		return menuRepository.findById(id)
+			.map(existingMenu -> {
+				existingMenu.setName(menuDto.getName());
+				existingMenu.setCategory(menuDto.getCategory());
+				existingMenu.setPrice(menuDto.getPrice());
+				existingMenu.setDescription(menuDto.getDescription());
+				return MenuDto.fromEntity(menuRepository.save(existingMenu));
+			});
 	}
 }
